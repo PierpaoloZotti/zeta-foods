10e4d9e4df79f275012d1a4fd4e8eadb3dd474bd
Pierpaolo Zotti <zotti.pierpaolo@outlook.it>
2024-04-29 15:20:05 -0300

    fix: left a clear project

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,5 @@ export default async function Home() {
       categories: true,
     },
   });
-  return (
-    <main className="flex flex-col gap-4">
-      {restaurantes.map((restaurante) => (
-        <div key={restaurante.id}>
-          <div className="flex items-center gap-x-8">
-            <div className="flex flex-col gap-y-2">
-              <h2>Nome: {restaurante.name}</h2>
-              <p>Taxa de entrega: {restaurante.deliveryFee.toString()}</p>
-            </div>
-            <div className="relative size-24  overflow-hidden rounded-xl shadow-xl">
-              <Image src={restaurante.imageUrl} alt={restaurante.name} fill />
-            </div>
-          </div>
-          <ul>
-            {restaurante.categories.map((category) => (
-              <li key={category.id}>{category.name}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </main>
-  );
+  return <main className="flex flex-col gap-4">zetafoods</main>;
 }

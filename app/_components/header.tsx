2d03014d0342e81e29c94aa9e0eda2d3454de84b
Pierpaolo Zotti <zotti.pierpaolo@outlook.it>
2024-05-02 10:55:08 -0300

    fix: ensure logo click redirect to homepage

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,14 @@
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="Zetafoods" width={100} height={30} />
+      <Link href="/">
+        <Image src="/logo.png" alt="Zetafoods" width={100} height={30} />
+      </Link>
       <Button
         className="border-none bg-transparent"
         variant="outline"

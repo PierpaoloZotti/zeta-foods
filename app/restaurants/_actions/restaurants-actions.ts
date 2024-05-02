6ae12752a73d668052f9f4ce93a35d233ed79dec
Pierpaolo Zotti <zotti.pierpaolo@outlook.it>
2024-05-02 10:51:33 -0300

    feat: add actions for restaurant queries

---
+++ app/restaurants/_actions/restaurants-actions.ts
@@ -0,0 +1,42 @@
+import prisma from "@/app/_lib/db";
+
+export const getRestaurants = async () => {
+  return await prisma.restaurant.findMany({
+    include: {
+      categories: true,
+    },
+  });
+};
+
+export const getRestaurantsById = async (id: string) => {
+  return await prisma.restaurant.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      categories: {
+        include: {
+          products: {
+            include: {
+              restaurant: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      products: {
+        take: 10,
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+};
